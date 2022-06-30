c2409eece83a632bb93c4508dcae8d61af959740
ashachakre0906 <chourpagar.asha@gmail.com>
2022-06-29 17:25:08 -0700

    wrote afunction which uses writeFileSync method to use promises instaed of call back function

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ const promptUser = () => {
             ,
             {
                 type : 'input' ,
-                name : 'name' ,
+                name : 'github' ,
                 message : 'What is your Github UserName ?'
             }
             ,
             {
                 type : 'input' ,
-                name : 'Linkedin' ,
+                name : 'linkedin' ,
                 message : 'Enter your Linkedin profile ?'
             },
 
@@ -36,3 +36,39 @@ const promptUser = () => {
         ]
     )
 }
+const generateHTML = ({name,location,hobby,github,linkedin}) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" type =text/css href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>portfolio generator</title>
+</head>
+<body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Hi! My name is ${name}</h1>
+      <p class="lead">I am from ${location}.</p>
+      <p class="lead">My hobbies are ${hobby}.</p>
+      <h3><span class="badge badge-secondary">Contact Me</span></h3>
+      <ul class="list-group">
+        <li class="list-group-item">My GitHub username is ${github}</li>
+        <li class="list-group-item">LinkedIn: ${linkedin}</li>
+      </ul>
+    </div>
+  </div>
+  </body>
+  </html>`;
+//Using writeFileSync as a promise
+
+const init = () => {
+    promptUser()
+       //Use writeFileSync method to use promises instead of a callback function
+       .then((answers) => fs.writeFileSync('./index.html', generateHTML(answers)))
+       .then(() => console.log ("sucessfully wrote to index.html"))
+       .catch((err) => console.error(err));
+};
+
+init();
